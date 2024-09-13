431b517e09519d486b724ca7b70cfbd962675a53
sasisam007 <sasisam007@gmail.com>
2024-09-13 19:38:38 +0530

    two way communication is implemented

--- socket_programming/basic_application/basiclient.java
+++ socket_programming/basic_application/basiclient.java
@@ -8,14 +8,34 @@ public class basiclient {
         Socket client=new Socket("127.0.0.1",1904);
         System.out.println("connection is established");
 
-        Scanner scanner=new Scanner(System.in);
-        System.out.println("Enter a meassage to send");
-        String message=scanner.nextLine();
-
+        Scanner scanner=new Scanner(client.getInputStream());
         OutputStream OutputStream=client.getOutputStream();
-        OutputStream.write((message+"\n").getBytes());
+        Scanner input=new Scanner(System.in);
+
+        while(true)
+        {
+            System.out.println("Enter a meesage to send");
+            String message=input.nextLine();
+            OutputStream.write((message+"\n").getBytes());
+            OutputStream.flush();
+            System.out.println("Message sent Waiting for reply");
+            if(message.equals("bye"))
+            {
+                break;
+            }
+
+            if(scanner.hasNextLine())
+            {
+                String incomingString=scanner.nextLine();
+                System.out.print("Server says : "+incomingString);
+                if(incomingString.equals("bye"))
+                {
+                    break;
+                }
+            }
+            
+        }
         //outputStream.write((message + "\n").getBytes()) converts the message into a byte array and sends it to the server. The \n ensures that the message is recognized as a complete line (similar to hitting Enter).
-        OutputStream.flush();
         client.close();
     }
 }
--- socket_programming/basic_application/basicserver.java
+++ socket_programming/basic_application/basicserver.java
@@ -1,4 +1,4 @@
-import java.io.IOException;
+import java.io.*;
 import java.net.*;
 import java.util.*;
 public class basicserver
@@ -12,12 +12,33 @@ public class basicserver
 
 
        Socket clientsocket=socket.accept();
-        try (Scanner scanner = new Scanner(clientsocket.getInputStream())) {
-            String message=scanner.nextLine();
+       Scanner scanner = new Scanner(clientsocket.getInputStream());       
+       Scanner send = new Scanner(System.in);       
+       OutputStream OutputStream=clientsocket.getOutputStream();
+        while(true)
+        {
+            if(scanner.hasNextLine())
+            {
+                String message=scanner.nextLine();
+                System.out.println("client says : "+message);
+                if(message.equals("bye"))
+                {
+                    System.out.println("Termination the section");
+                    break;
+                }
 
-            System.out.println("The client texted : "+message);
+                System.out.print("Enter a Message to send to client : ");
+                
+                String outputmessage=send.nextLine();
+                OutputStream.write((outputmessage+"\n").getBytes());
+                OutputStream.flush();
+                System.out.println("Message sent Waiting for reply");
+                if(outputmessage.equals("bye"))
+                {
+                    break;
+                }
+            }
         }
-        
         socket.close();
        clientsocket.close();
        System.out.println("Client is connected"); 
